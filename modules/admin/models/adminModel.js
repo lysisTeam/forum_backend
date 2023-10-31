e3626a7834ebcc99bbbcb12bdc0f20cfa223a43e
abdul-kodir2020 <mounirouabdul40@gmail.com>
2023-10-31 22:57:27 +0100

    save

---
+++ modules/admin/models/adminModel.js
@@ -0,0 +1,23 @@
+// const mongoose = require('mongoose')
+
+// const userSchema = new mongoose.Schema(
+//     {
+//         nom : {
+//             type: String,
+//             required
+//         },
+//         prenoms : {
+//             type: String,
+//             required
+//         },
+//         age: {
+//             type: Number,
+//             required
+//         }
+//     },
+//     {
+//         timestamps: true
+//     }
+// )
+
+// module.exports = mongoose.model('userModel', userSchema)
